b3237ede1959ae8b48b91883bdfac43f6824671b
Chris Lacina <clacina@mindspring.com>
2020-12-06 17:10:24 -0800

    Initial Checkin

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+
+    Ray lastRay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        NavMeshAgent nma = GetComponent<NavMeshAgent>();
+        nma.speed *= 2.0f;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            MoveToCursor();
+        }
+    }
+
+    private void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if(Physics.Raycast(ray, out hit))
+        {
+            Debug.Log("Point: " + hit.point);
+            NavMeshAgent nma = GetComponent<NavMeshAgent>();
+            nma.SetDestination(hit.point);
+
+        }
+    }
+}
